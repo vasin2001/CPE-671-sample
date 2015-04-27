df61825dadef299b0157fc0bbacdcae48866a6ca
vasin2001 <vasin2001@hotmail.com>
2015-04-27 19:00:09 +0700

    Update Startup Test Routine
    Test Factorial Function Added

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
-// For vim user compile with : :w !gcc main.c -o main
+// For vim user compile with : :w !gcc *.c -o main
+// For cmd.exe compile with : gcc *.c -o main
 
-
+// Program Usage : main.exe (command) (value)
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,10 +11,23 @@
 #include "product.h"
 
 
-int main (void) {
+int main ( int arc, char **argv) {
+	// Loop Counter
+	int i;
+	
+	// Welcome Message
+	printf("Simple Calculator Software\r\n");
+	
+	// Test Factorial Function
+	printf("Testing : Factorial Function\r\n");
+	
+	for (i = 0; i < 11; i++) {
+		printf("\r\nfindFac %d -> %d\r\n", i, findFac(i));
+	}
+	
+	printf("%s", argv[1]);
+
+
 
 	return 0;
 }
-
-
-
