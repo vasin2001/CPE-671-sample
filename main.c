977d12cc0503bb21d66c8328946c90033d3528e5
vasin2001 <vasin2001@hotmail.com>
2015-04-27 18:31:41 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+// For vim user compile with : :w !gcc main.c -o main
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "factorial.h"
+#include "sum.h"
+#include "difference.h"
+#include "product.h"
+
+
+int main (void) {
+
+	return 0;
+}
+
+
+
